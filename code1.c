d4e82870da927a77c95f8be7b7b16d4b7de06c0f
mehemmedzamanov <mehemmedzamann@gmail.com>
2026-02-18 01:49:33 +0400

    Create code1.c with a basic C program
    Add a simple C program that prints 'Salam'.

---
+++ code1.c
@@ -0,0 +1,10 @@
+//CODE_01
+
+# include <stdio.h>
+# include <conio.h>
+int main(){
+    printf("Salam"); //qisa kod izahi
+    getch();
+/*uzun yazi qeydleri ucun
+CONIO.H funksiyasi mutleq lazimdir getch() funksiyasi ucun*/
+}
